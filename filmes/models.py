6159968fa383a96b98bd876316796c482534dd5f
renascm_ <renatochagas.m@gmail.com>
2025-03-21 20:20:30 -0300

    feat: rename fields in Filme and FilmeAssistido models for consistency

--- filmes/models.py
+++ filmes/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Filme(models.Model):
-    Titulo = models.CharField(max_length=255)
+    Title = models.CharField(max_length=255)
     
-    def __str__ (self):
-        return f'{self.Titulo}'
+    def __str__(self):
+        return f'{self.Title}'
 
 class FilmeAssistido(models.Model):
-    Titulo = models.ForeignKey(Filme, on_delete=models.CASCADE)
-    Analise = models.TextField(null=True,blank=True)
+    Title = models.ForeignKey(Filme, on_delete=models.CASCADE)  # 
+    Analise = models.TextField(null=True, blank=True)
     
-    def __str__ (self):
-        return f'{self.Titulo}'
+    def __str__(self):
+        return f'{self.Title}'
