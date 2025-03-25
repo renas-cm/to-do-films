f50b466d51efe80eaa51e3cb8e5497579aefff98
renascm_ <renatochagas.m@gmail.com>
2025-03-25 06:35:27 -0300

    feat: enhance filtering and searching in Filme viewset

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'filmes',
     'corsheaders',
-    'rest_framework',
     'django_filters',
 ]
 
--- filmes/views.py
+++ filmes/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 
 from .models import Filme
@@ -11,8 +11,8 @@ from .serializers import FilmeSerializer
 class FilterFilmeViewSet(viewsets.ModelViewSet):
     queryset = Filme.objects.all()
     serializer_class = FilmeSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ["Title","Watched"]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ["Title", "Watched"]
     search_fields = ["Watched"]
     
     
